d8b293ce2a2944d7f4d2fdd916ba1f9e05ad3045
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-08-05 16:38:58 +0900

    add user and stock association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,15 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
-  def self.new_form_lookup(ticker_sysbol)
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+
+  end
+
+  def self.new_from_lookup(ticker_symbol)
     begin
-      looked_up_stock = StockQuote::Stock.quote(ticker_sysbol)
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
       price = strip_commas(looked_up_stock.latest_price)
       new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
     rescue Exception => e
