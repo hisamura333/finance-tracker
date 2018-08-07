e4ed9af399695604a4290bab1071d650386afbe3
Taiki Hisamura <taiki@fridaynight.co.jp>
2018-08-07 21:58:54 +0900

    create add/remove riend

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'my_friends', to: "users#my_friends"
+  get 'search_friends', to: "users#search"
   get 'serach_stocks', to: "stocks#search"
   resources :user_stocks, only: [:create, :destroy]
   resources :users, only: [:show]
   resources :friendships
+
+  post 'add_friend', to: 'users#add_friend'
 
 
   resources :welcome, only: [:index]
